f31f5ff2543d813cd7662e8563362267073ae651
KillerJacKTM05 <dogukankvrak@hotmail.com>
2021-11-21 19:29:52 +0300

    Anons

---
+++ Codes/Pumpkin Jam/Assets/MetroSound/Metro/Anons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anons : MonoBehaviour
+{
+    public AudioClip anons;
+
+    void Start()
+    {
+        StartCoroutine(Ses());
+    }
+
+    IEnumerator Ses()
+    {
+        AudioSource.PlayClipAtPoint(anons, transform.position, 1);
+        yield return new WaitForSeconds(300);
+    }
+}
