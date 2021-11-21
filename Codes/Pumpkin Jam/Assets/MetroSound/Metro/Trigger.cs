8e144b5a4f0e65c4114ad743b56fa394e2df9023
KillerJacKTM05 <dogukankvrak@hotmail.com>
2021-11-21 18:50:00 +0300

    Trigger Sound

---
+++ Codes/Pumpkin Jam/Assets/MetroSound/Metro/Trigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public int index = 1;
+    public bool isTriggered = false;
+    public bool played = false;
+    [SerializeField]public AudioClip[] sound = new AudioClip[1];
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = false;
+            played = false;
+        }
+    }
+    private void Update()
+    {
+        PlaySound();
+    }
+    private void PlaySound()
+    {
+        if (isTriggered && !played)
+        {
+            for(int i = 0; i < index; i++)
+            {
+                AudioSource.PlayClipAtPoint(sound[i], transform.position, 1);
+            }
+            played = true; 
+        }
+        else
+        {
+
+        }
+    }
+}
